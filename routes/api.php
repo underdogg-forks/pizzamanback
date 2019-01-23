950ca1a5fba75b234677f7f6fdb4785b168e3e6d
Niels Drost <nielsdrost7@gmail.com>
2019-01-23 21:17:13 +0100

    some fixes

--- routes/api.php
+++ routes/api.php
@@ -17,24 +17,31 @@ Route::middleware('jwt.auth')->get('users', function () {
     return auth('api')->user();
 });
 
-Route::post('login', 'APILoginController@login');
-
-// Route to create a new role
-Route::post('role', 'JwtAuthenticateController@createRole');
-// Route to create a new permission
-Route::post('permission', 'JwtAuthenticateController@createPermission');
-// Route to assign role to user
-Route::post('assign-role', 'JwtAuthenticateController@assignRole');
-// Route to attache permission to a role
-Route::post('attach-permission', 'JwtAuthenticateController@attachPermission');
-
-
 // API route group that we need to protect
-Route::group(['prefix' => 'api', 'middleware' => ['ability:admin,create-users']], function()
+Route::group(['prefix' => 'v1', 'middleware' => ['ability:admin,create-users']], function()
 {
+
+    // Route to create a new role
+    Route::post('role', 'Auth\JwtAuthenticateController@createRole');
+    // Route to create a new permission
+    Route::post('permission', 'Auth\JwtAuthenticateController@createPermission');
+    // Route to assign role to user
+    Route::post('assign-role', 'Auth\JwtAuthenticateController@assignRole');
+    // Route to attache permission to a role
+    Route::post('attach-permission', 'Auth\JwtAuthenticateController@attachPermission');
+
+
+
     // Protected route
-    Route::get('users', 'JwtAuthenticateController@index');
+    Route::get('users', 'Auth\JwtAuthenticateController@index');
 });
 
 // Authentication route
-Route::post('authenticate', 'JwtAuthenticateController@authenticate');
+Route::group(['prefix' => 'v1'], function()
+{
+    // Protected route
+    Route::post('login', 'Auth\JwtAuthenticateController@authenticate');
+    //Route::post('login', 'APILoginController@login');
+});
+
+
